5f582725d03c912cbdbd97d88dabbf177d2f5e2f
Olivier Gerbé <olivier.gerbe@hec.ca>
2015-11-24 10:00:38 -0500

    ajout d'une variable pour distiguer le site d'accueil

--- application/js/_init.js
+++ application/js/_init.js
@@ -4,17 +4,36 @@ var application_date = "2015-11-04";
 //----------------------------------
 var appliname = 'iut2';
 //----------------------------------
-//=== eportfolium
-var elgg_url_base = '../../../elgg112/';
+var karuta_site = 'iut2'; //-- eportfolium iut2 savoie
+//---------
+var bckname = '';
+var serverBCK = "";
+var serverFIL = "";
+var serverVER = "";
+//---------
+var elgg_url_base = '';
 var elgg_url_absolute = '';
 var elgg_auth_cas = false;
-//=== IUT 2
-//var elgg_url_base = 'elgg/';
-//var elgg_url_absolute = 'http://eportfolio.iut2.upmf-grenoble.fr/elgg/';
-//var elgg+auth_cas = false;
-
 var elgg_installed = true;
 var g_elgg_refreshing = 120000; // 120s 
+//---------
+if (karuta_site == 'eportfolium') {
+	serverBCK = "iut2-backend/rest/api";
+	serverFIL = "iut2-backend";
+	serverVER = "iut2-backend";
+	//---------
+	elgg_url_base = '../../../elgg112/';
+}
+if (karuta_site == 'iut2') {
+	bckname = '-iut2';
+	serverBCK = "karuta-backend"+bckname+"/rest/api";
+	serverFIL = "karuta-backend"+bckname;
+	serverVER = "karuta-backend"+bckname;
+	//---------
+	elgg_url_base = 'elgg/';
+	elgg_url_absolute = 'http://eportfolio.iut2.upmf-grenoble.fr/elgg/';
+	elgg_auth_cas = true;
+}
 //----------------------------------
 var languages = [];
 var languages_name = [];
@@ -22,25 +41,9 @@ languages [0] = 'fr';
 languages_name ['fr'] = 'Français';
 languages [1] = 'en';
 languages_name ['en'] = 'English';
-
 //----------------------------------
 var NONMULTILANGCODE = 0;  // default language if non-multilingual
 var LANGCODE = 0; //default value
 var LANG = languages[LANGCODE]; //default value
-//----------------------------------
-
-//=== eportfolium
-var bckname = '';
-var serverBCK = "iut2-backend/rest/api";
-var serverFIL = "iut2-backend";
-var serverVER = "iut2-backend";
-
-/*
-//=== IUT 2
-var bckname = '-iut2';
-var serverBCK = "karuta-backend"+bckname+"/rest/api";
-var serverFIL = "karuta-backend"+bckname;
-var serverVER = "karuta-backend"+bckname;
-*/
 //----------------------------------
 var technical_support = "help.karuta@gmail.com";
